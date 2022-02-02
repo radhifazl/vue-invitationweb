0d782fce609cd1be48132ae9a37506d623108d05
radhifazli <radhifazlinurfahriza@gmail.com>
2022-02-02 19:44:48 +0700

    Berhasil menambahkan slider

--- src/components/BukuTamu.vue
+++ src/components/BukuTamu.vue
@@ -1,81 +1,221 @@
 <template>
   <div class="bukutamu-section container-fluid wood-bg" style="height: 1000px">
-      <div class="bukutamu-wrapper wrapper-z-idx">
-        <div class="bukutamu-title text-center">
-            <h1 class="font-title">Buku Tamu</h1>
-            <p class="font-sub">Berikan Doa / Ucapan Terbaik Anda Kepada Kedua Mempelai</p>
-        </div>
+    <div class="bukutamu-wrapper container-fluid wrapper-z-idx">
+      <div class="bukutamu-title text-center">
+        <h1 class="font-title">Buku Tamu</h1>
+        <p class="font-sub">
+          Berikan Doa / Ucapan Terbaik Anda Kepada Kedua Mempelai
+        </p>
+      </div>
 
-        <div class="buku-tamu mt-5 p-4">
-            <form>
-                <div class="box-nama mb-3">
-                    <input 
-                        type="text" class="form-control"
-                        name="nama" 
-                        id="nama" 
-                        placeholder="Nama anda" 
-                        required
-                    >
-                </div>
+      <div class="buku-tamu mx-auto mt-5 p-4">
+        <div class="form-wrapper">
+          <form>
+            <div class="box-nama mb-3">
+              <input
+                type="text"
+                class="form-control"
+                name="nama"
+                id="nama"
+                placeholder="Nama anda"
+                required
+              />
+            </div>
 
-                <div class="box-pesan mb-3">
-                    <textarea 
-                        name="pesan" class="form-control"
-                        id="pesan" 
-                        placeholder="Berikan ucapan & doa" 
-                        required style="resize: none;"></textarea>
-                </div>
+            <div class="box-pesan mb-4">
+              <textarea
+                name="pesan"
+                class="form-control"
+                id="pesan"
+                placeholder="Berikan ucapan & doa"
+                required
+                style="resize: none"
+              ></textarea>
+            </div>
 
-                <div class="box-buttons d-flex justify-content-around align-items-center">
-                    <div class="absence-box box-hadir">
-                        <input type="checkbox" name="hadir" id="hadir" @click="onlyOne(this)">
-                        <label for="hadir">Hadir</label>
-                    </div>
+            <div
+              class="box-buttons d-flex justify-content-between align-items-center"
+            >
+              <div class="absence-box box-hadir">
+                <input type="radio" name="absence" id="hadir" required />
+                <label for="hadir">Hadir</label>
+              </div>
 
-                    <div class="absence-box box-akanhadir">
-                        <input type="checkbox" name="akanhadir" id="akanhadir" @click="onlyOne(this)">
-                        <label for="akanhadir">Akan hadir</label>
-                    </div>
+              <div class="absence-box box-akanhadir">
+                <input type="radio" name="absence" id="akanhadir" required />
+                <label for="akanhadir">Akan hadir</label>
+              </div>
 
-                    <div class="absence-box box-tidakhadir">
-                        <input type="checkbox" name="tidakhadir" id="tidakhadir" @click="onlyOne(this)">
-                        <label for="tidakhadir">Tidak hadir</label>
-                    </div>
+              <div class="absence-box box-tidakhadir">
+                <input type="radio" name="absence" id="tidakhadir" required />
+                <label for="tidakhadir">Tidak hadir</label>
+              </div>
 
-                    <button type="submit" name="submit" class="submit-btn">Post</button>
-                </div>
-            </form>
+              <button type="submit" name="submit" class="submit-btn">
+                Post
+              </button>
+            </div>
+          </form>
         </div>
       </div>
+
+      <div class="slide-ucapan mt-5">
+        <vueper-slides
+          autoplay
+          :arrows="false"
+          fixed-height="200px"
+          class="no-shadow slides-wrapper"
+          slide-multiple
+          :visible-slides="5"
+          :gap="1"
+          :slide-ratio="1 / 3"
+          :dragging-distance="70"
+          :breakpoints="breakpoints"
+        >
+          <vueper-slide
+            class="slides"
+            v-for="slide in slides"
+            :key="slide.id"
+            :title="slide.nama"
+            :content="slide.pesan"
+          />
+        </vueper-slides>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+
 export default {
-    methods: {
-        onlyOne(checkbox) {
-            var checkboxes = document.getElementsByName('check')
-            checkboxes.forEach((item) => {
-                if (item !== checkbox) item.checked = false
-            })
-        }
-    }
-}
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  data: () => ({
+    slides: [
+      {
+        id: 1,
+        nama: "Riska",
+        pesan:
+          "Selamat menempuh bahtera rumah tangga yang bahagia. Jangan berantem perkara pencet odol dari tengah sama dari ujung, ya!",
+      },
+      {
+        id: 2,
+        nama: "Riska",
+        pesan:
+          "Selamat menempuh bahtera rumah tangga yang bahagia. Jangan berantem perkara pencet odol dari tengah sama dari ujung, ya!",
+      },
+      {
+        id: 3,
+        nama: "Erlangga",
+        pesan:
+          "Selamat atas janji pernikahannya hari ini. Semoga dengan bersatunya kalian dalam ikatan perkawinan menjadi titik awal perjalanan hidup bersama yang lebih membahagiakan.",
+      },
+      {
+        id: 4,
+        nama: "Mutiara",
+        pesan:
+          "Selamat menempuh hidup yang baru. Semoga pernikahannya menjadi awal yang membahagiakan.",
+      },
+      {
+        id: 5,
+        nama: "Erlangga",
+        pesan:
+          "Selamat atas janji pernikahannya hari ini. Semoga dengan bersatunya kalian dalam ikatan perkawinan menjadi titik awal perjalanan hidup bersama yang lebih membahagiakan.",
+      },
+    ],
+    breakpoints: {
+        600: {
+            visibleSlides: 3,
+        },
+    },
+  }),
+};
 </script>
 
 <style>
+.buku-tamu {
+  background: #c57752;
+  width: 500px;
+  height: 250px;
+  display: grid;
+  place-items: center;
+  border-radius: 9px;
+}
+
+@media screen and (max-width: 768px) {
     .buku-tamu {
-        background: #C57752;
-        border-radius: 9px;
+        width: 90%;
     }
+}
 
-    .absence-box {
-        display: flex;
-        align-items: center;
-        gap: 5px;
-    }
+.form-wrapper {
+  width: 100%;
+}
 
-    .radio-box input {
-        background-color: #C57752;
-    }
+.absence-box {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.absence-box {
+  width: 15px;
+  height: 15px;
+  cursor: pointer;
+  position: relative;
+  visibility: hidden;
+}
+
+.absence-box label {
+  visibility: visible;
+  font-size: 0.838rem;
+  white-space: nowrap;
+}
+
+.absence-box input::before,
+.absence-box input::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+
+.absence-box input::before {
+  background: white;
+  visibility: visible;
+}
+
+.absence-box input::after {
+  background: var(--secondary-color);
+  border: 2px solid white;
+  visibility: visible;
+  transform: scale(0);
+  transition: 0.3s ease;
+}
+
+.absence-box input:checked::after {
+  transform: scale(1);
+}
+
+.submit-btn {
+  border: none;
+  outline: none;
+  background: var(--secondary-color);
+  padding: 0.1rem 1.5rem;
+}
+
+.slides {
+  background: var(--secondary-color);
+}
+
+.vueperslide__content-wrapper:not(.vueperslide__content-wrapper--outside-top):not(.vueperslide__content-wrapper--outside-bottom) {
+  padding: 1rem;
+}
 </style>
--- src/components/GalleryPhoto.vue
+++ src/components/GalleryPhoto.vue
@@ -1,114 +1,120 @@
 <template>
-  <div class="gallery-photo container-fluid text-center wood-bg" style="height: 1000px">
-      <div class="gallery-wrapper wrapper-z-idx">
-          <div class="gallery-title">
-              <h1 class="font-title">Gallery Photos</h1>
-          </div>
-          
-          <div class="gallery-grid container mx-auto mt-5">
-              <div class="photos mx-auto">
-                  <div v-for="i in 7" :key="i" :class="'photo-items img-'+i">
-                  </div>
-              </div>
-          </div>
+  <div class="gallery-photo container-fluid text-center wood-bg">
+    <div class="gallery-wrapper wrapper-z-idx">
+      <div class="gallery-title">
+        <h1 class="font-title">Gallery Photos</h1>
       </div>
+
+      <div class="gallery-grid container mx-auto mt-5">
+        <div class="photos mx-auto">
+          <div v-for="i in 7" :key="i" :class="'photo-items img-' + i"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style>
-    
-    .modal-btn {
-        width: 10rem;
-        height: 2rem;
-        font-family: var(--first-font);
-        font-size: 1.2rem;
-        background: #B3541E;
-        border: none;
-        border-radius: 15px;
-        outline: none;
-        transition: all .5s ease;
+.gallery-photo {
+    height: 500px;
+}
+@media screen and (min-width: 768px) {
+    .gallery-photo {
+        height: 900px;
     }
+}
+.modal-btn {
+  width: 10rem;
+  height: 2rem;
+  font-family: var(--first-font);
+  font-size: 1.2rem;
+  background: #b3541e;
+  border: none;
+  border-radius: 15px;
+  outline: none;
+  transition: all 0.5s ease;
+}
 
-    .modal-btn:hover {
-        background: #c95b1b;
-    }
+.modal-btn:hover {
+  background: #c95b1b;
+}
 
-    .photos {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        grid-template-rows: auto;
-        grid-gap: 0.5rem;
-        grid-template-areas: 'img-1 img-2 img-2'
-                             'img-3 img-4 img-5'
-                             'img-6 img-6 img-7';
-    }
+.photos {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  grid-gap: 0.5rem;
+  grid-template-areas:
+    "img-1 img-2 img-2"
+    "img-3 img-4 img-5"
+    "img-6 img-6 img-7";
+}
 
-    .photos .photo-items {
-        min-width: 100px;
-        height: 120px;
-        background-position: center;
-        background-size: cover;
-        background-repeat: no-repeat;
-        position: relative;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-        border-radius: 1rem;
-        transition: all 0.35s ease;
-    }
+.photos .photo-items {
+  min-width: 100px;
+  height: 120px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  border-radius: 1rem;
+  transition: all 0.35s ease;
+}
 
-    .img-1 {
-        grid-area: img-1;
-        background: url("../assets/images/gallery/img-1.png");
-    }
+.img-1 {
+  grid-area: img-1;
+  background: url("../assets/images/gallery/img-1.png");
+}
 
-    .img-2 {
-        grid-area: img-2;
-        background: url("../assets/images/gallery/img-2.png");
-    }
+.img-2 {
+  grid-area: img-2;
+  background: url("../assets/images/gallery/img-2.png");
+}
 
-    .img-3 {
-        grid-area: img-3;
-        background: url("../assets/images/gallery/img-3.png");
-    }
+.img-3 {
+  grid-area: img-3;
+  background: url("../assets/images/gallery/img-3.png");
+}
 
-    .img-4 {
-        grid-area: img-4;
-        background: url("../assets/images/gallery/img-4.png");
-    }
+.img-4 {
+  grid-area: img-4;
+  background: url("../assets/images/gallery/img-4.png");
+}
 
-    .img-5 {
-        grid-area: img-5;
-        background: url("../assets/images/gallery/img-5.png");
-    }
+.img-5 {
+  grid-area: img-5;
+  background: url("../assets/images/gallery/img-5.png");
+}
 
-    .img-6 {
-        grid-area: img-6;
-        background: url("../assets/images/gallery/img-6.png");
-    }
+.img-6 {
+  grid-area: img-6;
+  background: url("../assets/images/gallery/img-6.png");
+}
 
-    .img-7 {
-        grid-area: img-7;
-        background: url("../assets/images/gallery/img-7.png");
-    }
+.img-7 {
+  grid-area: img-7;
+  background: url("../assets/images/gallery/img-7.png");
+}
 
-    @media screen and (min-width: 768px) {
-        .photos .photo-items {
-            min-width: 230px;
-            min-height: 180px;
-        }
-    }
+@media screen and (min-width: 768px) {
+  .photos .photo-items {
+    min-width: 230px;
+    min-height: 180px;
+  }
+}
 
-    @media screen and (min-width: 992px) {
-        .photos .photo-items {
-            min-width: 300px;
-            min-height: 250px;
-        }
-    }
+@media screen and (min-width: 992px) {
+  .photos .photo-items {
+    min-width: 300px;
+    min-height: 250px;
+  }
+}
 </style>
